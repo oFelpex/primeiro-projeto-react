a946381b114e7711073d95254a0244591abd681b
Felipe Costa da Silva <felipe95176@gmail.com>
2024-06-11 10:08:49 -0300

    Comecei a estilização do css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import logoImg from './assets/images/logo.png';
+import './styles/App.css';
 
 class App extends React.Component {
   render() {
@@ -6,14 +8,22 @@ class App extends React.Component {
       <header>
         <nav id="navBar">
           <div className="nav-brand">
-            <img src="" alt="" />
+            <img src={logoImg} alt="" />
             <h1>Space Flight News</h1>
           </div>
           <ul className="navList">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Trending</a></li>
-            <li><a href="#">Categories</a></li>
-            <li><a href="#">About</a></li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Trending</a>
+            </li>
+            <li>
+              <a href="#">Categories</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
           </ul>
         </nav>
       </header>
