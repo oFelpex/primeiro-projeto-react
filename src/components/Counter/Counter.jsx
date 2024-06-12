19e61b3d3bca2dd2bf1dd39795d133167fe4bd82
Felipe Costa <felipe95176@gmail.com>
2024-06-12 20:11:50 -0300

    continuando as aulas

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -3,18 +3,18 @@ import React from "react";
 export class Counter extends React.Component {
 constructor() {
     super();
-    this.contador = 0;
 
+    this.state = {contador: 0};
 
 }
 
     render() {
         return(
             <div style={{marginTop: '20px', marginLeft: '20px'}}>
-                <h1>{this.contador}</h1>
+                <h1>{this.state.contador}</h1>
                 <div>
-                    <button style={{marginRight: '10px'}} onClick={() => { this.contador-- }}>Diminuir</button>
-                    <button onClick={() => { this.contador++ }}>Aumentar</button>
+                    <button style={{marginRight: '10px'}} onClick={() => { this.setState({contador: this.state.contador-1}) }}>Diminuir</button>
+                    <button onClick={() => { this.setState({contador: this.state.contador+1}) }}>Aumentar</button>
                 </div>
             </div>
         )
